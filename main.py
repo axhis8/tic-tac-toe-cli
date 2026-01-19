30aaa108358e9c942ee686583ec61bbb5b7a6acf
ashis <161737873+axhis8@users.noreply.github.com>
2026-01-19 13:45:02 +0100

    Initialize documents

---
+++ main.py
@@ -0,0 +1,5 @@
+from ttt import TicTacToe
+
+if __name__ == '__main__':
+    tic_tac_toe = TicTacToe()
+    tic_tac_toe.start()
---
+++ ttt.py
@@ -0,0 +1,30 @@
+import random
+
+class TicTacToe():
+
+    def __init__(self):
+        pass
+
+    
+    # =================== LOGIC ===================
+    def get_computer_pos(self):
+        pass
+
+    def get_human_pos(self):
+        pass
+
+    def check_win(self, human, computer):
+        pass
+
+    def play(self):
+        pass
+    
+    def start(self):
+        self.play()
+
+    # =================== UI ===================
+    def print_menu(self):
+        pass
+
+    def print_board(self):
+        pass
