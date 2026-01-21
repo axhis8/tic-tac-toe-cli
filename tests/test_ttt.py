a1f67b233eba3a554f3599ac20de95d6e9b4a2ec
ashis <161737873+axhis8@users.noreply.github.com>
2026-01-21 14:49:53 +0100

    Add new unit tests
    Added a get_empty_pos test
    
    Added a evaluate test
    
    Added a minimax test

--- tests/test_ttt.py
+++ tests/test_ttt.py
@@ -2,7 +2,6 @@ from src.tictactoe import TicTacToe
 from src.constants import Ending
 import pytest
 
-
 class TestCheckWinX:
 
     @pytest.mark.parametrize("board", [
@@ -39,7 +38,7 @@ class TestCheckWinX:
           "X", "O", "-"])
     ])
 
-    def test_check_win(self, board) -> None:
+    def test_check_win_X(self, board: list) -> None:
         assert TicTacToe.check_win(board) == Ending.X_WON
 
 
@@ -79,7 +78,7 @@ class TestCheckWinO:
           "O", "X", "-"])
     ])
 
-    def test_check_win_diagonal_second(self, board) -> None:            
+    def test_check_win_O(self, board: list) -> None:            
         assert TicTacToe.check_win(board) == Ending.O_WON
     
 
@@ -111,5 +110,77 @@ class TestGetBoard:
           "X | O | X\n "),
     ])
     
-    def test_get_board_empty(self, board, result_board) -> None:
+    def test_get_board(self, board: list, result_board: str) -> None:
         assert TicTacToe.get_board(board) == "\n " + result_board
+
+
+class TestGetEmptyPos:
+    
+    @pytest.mark.parametrize("board, result", [
+        
+        (["-", "-", "-", # empty board, should return full list
+          "-", "-", "-",
+          "-", "-", "-"],
+          list(range(0, 9))), 
+        
+        (["O", "-", "O", 
+          "-", "X", "-",
+          "X", "O", "-"],
+          [1, 3, 5, 8]), 
+
+        (["X", "X", "O", # full board, should return empty list
+          "O", "O", "X",
+          "X", "O", "O"],
+          []), 
+
+    ])
+
+    def test_get_empty_pos(self, board: list, result: list) -> None:
+        assert TicTacToe.get_empty_pos(board) == result
+
+
+class TestEvaluate:
+    @pytest.mark.parametrize("board, ai_symbol, human_symbol, result", [
+        
+        (["O", "O", "O",
+          "X", "O", "X",
+          "O", "X", "X"], 
+          "O", "X", 10),
+
+        (["X", "O", "O",
+          "O", "X", "X",
+          "O", "X", "O"], 
+          "X", "O", 0),
+
+        (["X", "O", "O",
+          "X", "X", "O",
+          "O", "X", "O"], 
+          "X", "O", -10),
+
+    ])
+
+    def test_evaluate(self, board: list, ai_symbol: str, human_symbol: str, result: int) -> None:
+        assert TicTacToe.evaluate(board, ai_symbol, human_symbol) == result
+
+
+class TestMinimax:
+      
+    # in the test method, is_maximizing is given False. Thus simulating that the next move would be the players.
+    @pytest.mark.parametrize("board, result", [ 
+        
+        (["O", "-", "O",
+          "O", "X", "X",
+          "-", "-", "X"], 10),
+
+        (["O", "X", "O",
+          "O", "-", "X",
+          "X", "O", "X"], 0),
+
+        (["O", "-", "O",
+          "O", "X", "X",
+          "X", "-", "X"], -10),
+
+    ])
+
+    def test_minimax(self, board: list, result: int) -> None:
+        assert TicTacToe.minimax(board, "O", "X", False) == result
