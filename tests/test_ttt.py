63fe2406237922bc5c28df78b396f1b749a23203
ashis <161737873+axhis8@users.noreply.github.com>
2026-01-21 13:50:38 +0100

    Update tests by introducing parametrize

--- tests/test_ttt.py
+++ tests/test_ttt.py
@@ -1,133 +1,115 @@
 from src.tictactoe import TicTacToe
 from src.constants import Ending
+import pytest
+
 
 class TestCheckWinX:
 
-    def test_check_win_horizontal_top(self) -> None:
-        board = ["X", "X", "X",
-                 "-", "-", "O",
-                 "-", "-", "O"]
-        assert TicTacToe.check_win(board) == Ending.X_WON
+    @pytest.mark.parametrize("board", [
+        (["X", "X", "X", # horizontal top
+          "-", "-", "O",
+          "-", "-", "O"]),
 
-    def test_check_win_horizontal_middle(self) -> None:
-        board = ["O", "O", "X",
-                 "X", "X", "X",
-                 "O", "-", "O"]
-        assert TicTacToe.check_win(board) == Ending.X_WON
+        (["O", "O", "X", # horizontal middle
+          "X", "X", "X",
+          "O", "-", "O"]),
 
-    def test_check_win_horizontal_bottom(self) -> None:
-        board = ["X", "O", "X",
-                 "O", "-", "O",
-                 "X", "X", "X"]
-        assert TicTacToe.check_win(board) == Ending.X_WON
+        (["X", "O", "X", # horizontal bottom
+          "O", "-", "O",
+          "X", "X", "X"]),
 
-    def test_check_win_vertical_first(self) -> None:
-        board = ["X", "O", "X",
-                 "X", "-", "O",
-                 "X", "O", "O"]
-        assert TicTacToe.check_win(board) == Ending.X_WON
+        (["X", "O", "X", # vertical left
+          "X", "-", "O",
+          "X", "O", "O"]),
 
-    def test_check_win_vertical_second(self) -> None:
-        board = ["O", "X", "-",
-                 "O", "X", "O",
-                 "X", "X", "O"]
-        assert TicTacToe.check_win(board) == Ending.X_WON
+        (["O", "X", "-", # vertical middle
+          "O", "X", "O",
+          "X", "X", "O"]),
 
-    def test_check_win_vertical_third(self) -> None:
-        board = ["X", "O", "X",
-                 "O", "O", "X",
-                 "O", "X", "X"]
-        assert TicTacToe.check_win(board) == Ending.X_WON
-    
-    def test_check_win_diagonal_first(self) -> None:
-        board = ["X", "O", "X",
-                 "O", "X", "O",
-                 "O", "O", "X"]
-        assert TicTacToe.check_win(board) == Ending.X_WON
+        (["X", "O", "X", # vertical right
+          "O", "O", "X",
+          "O", "X", "X"]),
 
-    def test_check_win_diagonal_second(self) -> None:
-        board = ["X", "O", "X",
-                 "-", "X", "O",
-                 "X", "O", "-"]
+        (["X", "O", "X", # diagonal right
+          "O", "X", "O",
+          "O", "O", "X"]),
+
+        (["X", "O", "X", # diagonal left
+          "-", "X", "O",
+          "X", "O", "-"])
+    ])
+
+    def test_check_win(self, board) -> None:
         assert TicTacToe.check_win(board) == Ending.X_WON
 
 
 class TestCheckWinO:
 
-    def test_check_win_horizontal_top(self) -> None:
-        board = ["O", "O", "O",
-                 "-", "-", "X",
-                 "-", "-", "X"]
-        assert TicTacToe.check_win(board) == Ending.O_WON
+    @pytest.mark.parametrize("board", [
+        (["O", "O", "O", # horizontal top
+          "-", "-", "X",
+          "-", "-", "X"]),
 
-    def test_check_win_horizontal_middle(self) -> None:
-        board = ["X", "X", "O",
-                 "O", "O", "O",
-                 "X", "-", "X"]
-        assert TicTacToe.check_win(board) == Ending.O_WON
+        (["X", "X", "O", # horizontal middle
+          "O", "O", "O",
+          "X", "-", "X"]),
 
-    def test_check_win_horizontal_bottom(self) -> None:
-        board = ["O", "X", "O",
-                 "X", "-", "X",
-                 "O", "O", "O"]
-        assert TicTacToe.check_win(board) == Ending.O_WON
+        (["O", "X", "O", # horizontal bottom
+          "X", "-", "X",
+          "O", "O", "O"]),
 
-    def test_check_win_vertical_first(self) -> None:
-        board = ["O", "X", "O",
-                 "O", "-", "X",
-                 "O", "X", "X"]
-        assert TicTacToe.check_win(board) == Ending.O_WON
+        (["O", "X", "O", # vertical left
+          "O", "-", "X",
+          "O", "X", "X"]),
 
-    def test_check_win_vertical_second(self) -> None:
-        board = ["X", "O", "-",
-                 "X", "O", "X",
-                 "O", "O", "X"]
-        assert TicTacToe.check_win(board) == Ending.O_WON
+        (["X", "O", "-", # vertical middle
+          "X", "O", "X",
+          "O", "O", "X"]),
 
-    def test_check_win_vertical_third(self) -> None:
-        board = ["O", "X", "O",
-                 "X", "X", "O",
-                 "X", "O", "O"]
-        assert TicTacToe.check_win(board) == Ending.O_WON
+        (["O", "X", "O", # vertical right
+          "X", "X", "O",
+          "X", "O", "O"]),
 
-    def test_check_win_diagonal_first(self) -> None:
-        board = ["O", "X", "O",
-                 "X", "O", "X",
-                 "X", "X", "O"]
-        assert TicTacToe.check_win(board) == Ending.O_WON
+        (["O", "X", "O", # diagonal right
+          "X", "O", "X",
+          "X", "X", "O"]),
 
-    def test_check_win_diagonal_second(self) -> None:
-        board = ["O", "X", "O",
-                 "-", "O", "X",
-                 "O", "X", "-"]
+        (["O", "X", "O", # diagonal left
+          "-", "O", "X",
+          "O", "X", "-"])
+    ])
+
+    def test_check_win_diagonal_second(self, board) -> None:            
         assert TicTacToe.check_win(board) == Ending.O_WON
     
 
 class TestGetBoard:
-    def test_get_board_empty(self) -> None:
-        board: list[str] = ["-", "-", "-",
-                            "-", "-", "-",
-                            "-", "-", "-"]
-        
-        result_board: str = f'- | - | -\n ' * 3
-        assert TicTacToe.get_board(board) == "\n " + result_board
-    
-    def test_get_board_X_example(self) -> None:
-        board: list[str] = ["X", "-", "-",
-                            "-", "X", "-",
-                            "O", "O", "X"]
-        
-        result_board: str = "X | - | -\n " \
-                            "- | X | -\n " \
-                            "O | O | X\n "
-        assert TicTacToe.get_board(board) == "\n " + result_board
 
-    def test_get_board_X_full(self) -> None:
-        board: list[str] = ["O", "O", "O",
-                            "O", "X", "X",
-                            "X", "O", "X"]
-        
-        result_board: str = "O | O | O\n " \
-                            "O | X | X\n " \
-                            "X | O | X\n "
+    @pytest.mark.parametrize("board, result_board", [
+        (["-", "-", "-", # empty
+          "-", "-", "-",
+          "-", "-", "-"],
+
+          "- | - | -\n " \
+          "- | - | -\n " \
+          "- | - | -\n "),
+
+        (["X", "-", "-", # normal
+          "-", "X", "-",
+          "O", "O", "X"],
+
+          "X | - | -\n " \
+          "- | X | -\n " \
+          "O | O | X\n "),
+
+        (["O", "O", "O", # full
+          "O", "X", "X",
+          "X", "O", "X"],
+
+          "O | O | O\n " \
+          "O | X | X\n " \
+          "X | O | X\n "),
+    ])
+    
+    def test_get_board_empty(self, board, result_board) -> None:
         assert TicTacToe.get_board(board) == "\n " + result_board
